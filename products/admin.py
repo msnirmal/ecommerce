1f6776cf2efbd9313d9f5fb50966da83a69fa9f7
Nirmal Kumar Subramani <kumar.n.subramani@gmail.com>
2024-07-19 17:48:18 +0000

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',         
+        'name',
+        'category',
+        'price',
+        'image',
+    )
 
-admin.site.register(Category)
-admin.site.register(Product)
+    ordering = ('sku',)
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 
+        'friendly_name',
+    ) 
+    
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+
 
--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)     
 
