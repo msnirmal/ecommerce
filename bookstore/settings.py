4865d8d09acbe1cbd927e633073f1fbc0476d052
Nirmal Kumar Subramani <kumar.n.subramani@gmail.com>
2024-07-23 17:25:58 +0000

    working on an issue related to allowed host

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://bookstore2024-0169896f0da0.herokuapp.com/']
+ALLOWED_HOSTS = ['bookstore2024-0169896f0da0.herokuapp.com/']
 
 
 # Application definition
